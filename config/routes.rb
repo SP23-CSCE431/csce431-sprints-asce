5b3f46af3b875835ff6917cf96d888b010564c34
SebastianReyes19 <sebastian_reyes50@tamu.edu>
2023-02-12 07:56:34 +0000

    fix: changing the root for the app dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'dashboards#show'
+  root 'dashboards#show'
   devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
   devise_scope :admin do
     get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
