7c1d6babe3a0a7d3a49db55f9369de4d04ff842d
SebastianReyes19 <sebastian_reyes50@tamu.edu>
2023-02-12 07:42:15 +0000

    fix: migrating sources of the user.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_02_12_060930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+  end
 
   create_table "event_types", force: :cascade do |t|
     t.integer "points"
@@ -59,16 +68,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_12_060930) do
     t.integer "role_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    
-  create_table "admins", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "full_name"
-    t.string "uid"
-    t.string "avatar_url"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    
   end
 
 end
