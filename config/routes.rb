731bdaea87b02e64293fcc1f07dda4b550b063ec
Anthony Dao <adao102@tamu.edu>
2023-02-12 01:32:45 -0600

    fixed routes.rb root error

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :user_events, :event_types, :events, :roles
   # Defines the root path route ("/")
-  root 'users#index'
+  # root 'users#index'
   # root "articles#index"
   resources :users do
     member do
