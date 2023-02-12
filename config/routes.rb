4436144ee10b1afef4ad3fb9113304107350fae0
Anthony Dao <adao102@tamu.edu>
2023-02-12 01:58:31 -0600

    edited routes.rb for root issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'dashboards#show'
+  root 'dashboards#show'
   devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
   devise_scope :admin do
     get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # resources :user_events, :event_types, :events, :roles
   # Defines the root path route ("/")
-  # root 'users#index'
-  # root "articles#index"
   resources :users do
     member do
       get :delete
