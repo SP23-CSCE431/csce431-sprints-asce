f8bcad96dd0e22dd28ecccba60d1f7af99016588
SebastianReyes19 <sebastian_reyes50@tamu.edu>
2023-02-12 23:20:55 +0000

    minor typo in validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   has_many :roles, class_name: 'role', foreign_key: 'role_id'
-  validate :first_name, :last_name, :uin, :email, :phone_number, :dob, :points, :role_id
+  validates :first_name, :last_name, :uin, :email, :phone_number, :dob, :points, :role_id, presence: true
 end
