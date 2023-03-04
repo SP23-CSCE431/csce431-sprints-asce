31fe8bc42d73a644ab4f4361b63f3ce3ae9a0b1a
vpranav29 <vpranav@hotmail.com>
2023-03-04 17:40:34 +0000

    successfully allow users to sign up for events

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -2,22 +2,29 @@ class UserEventsController < ApplicationController
   before_action :set_user_event, only: %i[show edit update destroy]
 
   # GET /user_events or /user_events.json
+  # the index path simply displays all user_events ie. all events that are specifically signed up by a user and tied to them
   def index
     @user_events = UserEvent.all
   end
 
   # GET /user_events/1 or /user_events/1.json
+  # the show path shows a specific event that is tied to a user that they have signed up for
   def show; end
 
   # GET /user_events/new
+  # the new path is where users can add events they want to sign up for and the eventId variable is in the url so it can autofill the event id field of a specific
+  # event that the user wants to sign up for
   def new
     @user_event = UserEvent.new
+    @eventId = params[:eventId]
   end
 
   # GET /user_events/1/edit
   def edit; end
 
   # POST /user_events or /user_events.json
+  # this method actually creates the new user_event record and officially adds it to the table representing that the user has successfully signed up for an event
+  # input fields with the user id and event id that are autofilled for the user
   def create
     @user_event = UserEvent.new(user_event_params)
 
@@ -46,6 +53,7 @@ class UserEventsController < ApplicationController
   end
 
   # DELETE /user_events/1 or /user_events/1.json
+  # This method allows a user to successfully destroy a record of an event they signed up from the table, ie. unsign up for an event that is on record for their account
   def destroy
     @user_event.destroy
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # <!-- establishes that the sign in page will be the first page of the website -->
   root 'dashboards#show'
   #root 'users#index'
   devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
@@ -6,14 +7,26 @@ Rails.application.routes.draw do
     get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
     get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
   end
+  # <!-- /profile path will take users to their profile page -->
   match 'profile', to: 'users#profile', via: :get
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # resources :user_events, :event_types, :events, :roles
   # Defines the root path route ("/")
+  # <!-- This creates all the essential routes for the user_events entity -->
+  # <!-- This gives the ability to create new, edit, or delete records of this entity -->
+  resources :user_events do
+    get :delete
+  end
+  # <!-- This creates all the essential routes for the events entity -->
+  # <!-- This gives the ability to create new, edit, or delete records of this entity -->
   resources :events do
     get :delete
   end
+  # <!-- This creates all the essential routes for the roles entity -->
+  # <!-- This gives the ability to create new, edit, or delete records of this entity -->
   resources :roles
+  # <!-- This creates all the essential routes for the users entity -->
+  # <!-- This gives the ability to create new, edit, or delete records of this entity -->
   resources :users do
     member do
       get :delete
