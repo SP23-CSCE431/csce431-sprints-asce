895fb804fbace02c780dd1c021d80ea922ea5b97
root <root@localhost>
2023-03-21 15:43:33 +0000

    rebasing

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ApplicationRecord
   has_many :event_types, class_name: 'event_type', foreign_key: 'event_type_id'
   validates :start, :end, :status, :type_id, presence: true
+  
+  def self.ransackable_attributes(auth_object = nil)
+    [  "name" ]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    [  ""]
+  end
 end
