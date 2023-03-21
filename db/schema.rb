bd6a84261af9aeec301603fdef6a8594b4ec2d1c
root <root@localhost>
2023-03-21 15:43:33 +0000

    rebasing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_12_060930) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_025323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_12_060930) do
     t.integer "type_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "points", default: 0
   end
 
   create_table "roles", force: :cascade do |t|
@@ -55,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_12_060930) do
     t.integer "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "checked", default: false
   end
 
   create_table "users", force: :cascade do |t|
