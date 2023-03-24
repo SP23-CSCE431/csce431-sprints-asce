18d4a8a7a81ccfd62233582f700e5d2baad2dd78
George-Z-L <46754576+George-Z-L@users.noreply.github.com>
2023-03-24 14:46:53 -0500

    Revert "rebasing"
    This reverts commit 5c68f5c582291762117b23a6005ef30944c18ce2.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,4 @@
 class User < ApplicationRecord
   has_many :roles, class_name: 'role', foreign_key: 'role_id'
   validates :first_name, :last_name, :uin, :email, :phone_number, :dob, :points, :role_id, presence: true
-  has_many :user_events
-  has_many :events, through: :user_events
-
-  def self.ransackable_attributes(auth_object = nil)
-    [  "first_name", "last_name", "uin"]
-  end
-
-  def self.ransackable_associations(auth_object = nil)
-    [  ""]
-  end
-  
 end
