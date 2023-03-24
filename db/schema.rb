e96b4df6c96b51c1ffdf4ea0cf5e03b66e9cfe14
George-Z-L <46754576+George-Z-L@users.noreply.github.com>
2023-03-24 10:58:06 -0500

    Revert "Updating Create Event"
    This reverts commit e3a38b012081ef72f928677d995c55d4fb2bc7c9.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_24_014107) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_025323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,7 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_24_014107) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.integer "points", default: 0
-    t.string "description"
   end
 
   create_table "roles", force: :cascade do |t|
