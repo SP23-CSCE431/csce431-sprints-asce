938575170b7bbf85697e50c86ab60c4bbba0ff23
George-Z-L <46754576+George-Z-L@users.noreply.github.com>
2023-03-26 16:38:59 -0500

    Revert "Creating Event Sign in Page"
    This reverts commit 22bb77bca6df5f43ec7a47ad6fc3ed915e18938e.

--- db/migrate/20230320225345_add_name_and_points_to_events.rb
+++
@@ -1,6 +0,0 @@
-class AddNameAndPointsToEvents < ActiveRecord::Migration[7.0]
-  def change
-    add_column :events, :name, :string
-    add_column :events, :points, :integer, default: 0
-  end
-end
