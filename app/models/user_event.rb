bc91d43d49fff7bb8b59e28481f2bc29974cc3a1
vpranav29 <vpranav@hotmail.com>
2023-03-27 01:40:44 +0000

    fixed user_event display with regard to newly added Event attributes

--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -1,4 +1,5 @@
 class UserEvent < ApplicationRecord
   belongs_to :user
   belongs_to :event
+  validates :user_id, uniqueness: { scope: :event_id, message: "You have already signed up for this event" }
 end
