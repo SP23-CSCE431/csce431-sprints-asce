c7b1a1c6be1294c992ea539b3e0406eab0ff7b18
vpranav29 <70822517+vpranav29@users.noreply.github.com>
2023-03-26 19:45:38 -0500

    Revert "User event display fix"

--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -1,5 +1,4 @@
 class UserEvent < ApplicationRecord
   belongs_to :user
   belongs_to :event
-  validates :user_id, uniqueness: { scope: :event_id, message: "You have already signed up for this event" }
 end
