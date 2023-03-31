2e54996830c70f11a509faa7765523005d09ccde
root <root@localhost>
2023-03-31 14:48:26 +0000

    point reset and other changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,11 @@
 class PagesController < ApplicationController
   def event_sign_in
     @q = User.ransack(params[:q])
-    @users = @q.result(distinct: true)
+    @users = @q.result(distinct: true).order(created_at: :asc)
 
     @p = Event.ransack(params[:q])
-    @events = @p.result(distinct: true)
+    @events = @p.result(distinct: true).order(created_at: :asc)
+    @event_id=params[:event_id]
   end
 
   def about
@@ -25,6 +26,8 @@ class PagesController < ApplicationController
         user.save        
         user_event.checked = true
         user_event.save
+        event.count += 1
+        event.save
         flash[:notice] = 'The user checked in this event succesfully.'
       end      
     else
@@ -34,10 +37,23 @@ class PagesController < ApplicationController
       user_event = UserEvent.find_by(user_id: user_id, event_id: event_id)
       user_event.checked = true
       user_event.save
+      event.count += 1
+      event.save
       flash[:notice] = 'The user checked in this event succesfully.'      
     end    
   
-  redirect_to pages_event_sign_in_path
+  redirect_to pages_event_sign_in_path(event_id: event_id)
   end
 
+  def reset_user_points   
+
+  end
+
+  def post_reset_user_points      
+    user = User.find_by(uin: params[:uin])
+    user.points = 0
+    user.save
+    flash[:notice] = 'User points succesfully reset to zero.'
+    redirect_to pages_reset_user_points_path
+  end 
 end
